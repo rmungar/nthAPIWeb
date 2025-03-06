84e0671536a51ad18d62c2fb7fb377cbc234657a
rmungar <mgraul@iesrafaelalberti.es>
2025-03-06 01:01:12 +0100

    Prueba con strings

--- nthAPIWeb/Models/Player.cs
+++ nthAPIWeb/Models/Player.cs
@@ -3,6 +3,6 @@ namespace nthAPIWeb.models;
 public class Player
 {
     public int Id {get; set;}
-    public int LastCheckPoint { get; set; }
-    public int Deaths { get; set; }
+    public string LastCheckPoint { get; set; }
+    public string Deaths { get; set; }
 }
