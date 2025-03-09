107a23e16b60f4700bd09d48befdcad4991353af
rmungar <mgraul@iesrafaelalberti.es>
2025-03-09 13:30:03 +0100

    1

--- nthAPIWeb/Models/Player.cs
+++ nthAPIWeb/Models/Player.cs
@@ -6,6 +6,7 @@ namespace nthAPIWeb.models;
 public class Player
 {
     [BsonId]
+    [BsonRepresentation(BsonType.String)] // Mongo lo manejará como ObjectId
     public string? _id { get; set; }
     public int lastCheckPoint { get; set; }
     public int deaths { get; set; }
